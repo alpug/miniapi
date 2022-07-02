f2aca09c8507efeaac868ba3b0bca64408cab281
Alpug Turing <addyman99@gmail.com>
2022-07-02 16:42:15 +0100

    Added CRUD methods for Blog posts

--- app/controllers/blog.controller.js
+++ app/controllers/blog.controller.js
@@ -17,26 +17,110 @@ exports.create = (req, res) => {
         title: req.body.title,
         created: req.body.created,
         content: req.body.content,
-        public: request.body.public
+        public: request.body.public ? req.body.public : false
     }
+    //save blog to db
+    Blog.create(blog)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Unspecified error occurred while creating this Blog post."
+            });
+        });
 };
 
 exports.findAll = (req, res) => {
-
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.iLike]: `%{title}%` } } : null;
+    Blog.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Unspecified error occurred during retrieval of Blog posts."
+            })
+        })
 };
 
-exports.findOne = (rqe, res) => {
-
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    Blog.findByPk(id)
+        .then(data => {
+            if (data) {
+            res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find a Blog post with id = ${id}`
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Unspecified error occurred updating Blog with id = ${id}`
+            })
+        })
 };
 
 exports.update = (req, res) => {
-
+    const id = req.params.id;
+    Blog.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Blog post successfully updated."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Blog with id = ${id}`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Unspecified error updating Blog with id = ${id}`
+            })
+        })
 };
 
 exports.delete = (req, res) => {
-
+    const id = req.params.id;
+    Blog.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: `Blog with id = ${id} deleted successfully.`
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Blog with id = ${id}`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `Unspecified error occurred while trying to delete Blog with id = ${id}`
+            });
+        });
 };
 
 exports.findAllPublic = (req, res) => {
-
+    Blog.findAll({ where: { public: true } })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 
+                    err.message || "Unspecified error occurred retrieving all Blog posts."
+            });
+        });
 };
